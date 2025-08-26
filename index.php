1810567590f78cdc63514f7bd9ae8247a3e32686
Niki Brown <284173+nikibrown@users.noreply.github.com>
2025-08-26 16:12:14 -0700

    uinitial wordpress files

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php get_header();?>
+
+<main id="site-content" role="main">
+  <?php if (have_posts()) : while ( have_posts() ) : the_post();?>
+  <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <header class="entry-header">
+      <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    </header>
+    <div class="entry-content">
+      <?php the_content(); ?>
+    </div>
+  </article>
+  <?php endwhile; ?>
+  <?php endif;?>
+</main>
+
+<?php get_footer();?>
