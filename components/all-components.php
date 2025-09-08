3e214b8afafaeec063aa3b6f0b64824c1c20d922
Niki Brown <284173+nikibrown@users.noreply.github.com>
2025-09-08 10:27:34 -0700

    ambassadors and board templates

---
+++ components/all-components.php
@@ -0,0 +1,13 @@
+<?php
+if( have_rows('components') ):
+    while( have_rows('components') ): the_row();
+        if( get_row_layout() == 'cta-banner' ):
+            get_template_part('components/section-cta-banner');
+        elseif( get_row_layout() == 'text-section-1-column' ):
+            get_template_part('components/section-text-1-column');
+        elseif( get_row_layout() == 'photo-banner-full-width' ):
+            get_template_part('components/section-photo-banner-full-width');
+        endif;
+    endwhile;
+endif;
+?>
